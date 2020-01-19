6f2093f3cf6298bd28d6a9c8bd889af390f3e517
David Hunt <git@dnotes.net>
2020-01-18 19:39:23 -0800

    no more borat, just lorem ipsum

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,24 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
 	<title>Sacred Traditions - Interfaith Exploration: sacred-traditions.org</title>
 </svelte:head>
 
-<h1>Most Wonderful Success!</h1>
+<h1>Lorem ipsum</h1>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia laboriosam repellendus consequatur deleniti, ullam quaerat eligendi tenetur iure neque est. Rem ipsam perferendis dicta facere magni a autem quae minima!</p>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, excepturi facilis possimus consectetur, asperiores at nihil enim veniam repellendus officiis recusandae ratione corporis error dolor iste quis ex! Voluptatum, dignissimos.</p>
+
+<h2>Ipsum dolor</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor aut cum perspiciatis voluptatem, ea necessitatibus animi quaerat fuga accusamus laboriosam, maxime laudantium dolorum, quam dolorem ipsum est mollitia voluptates. Repudiandae?</p>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil dolor numquam eveniet unde sequi dignissimos suscipit magni atque sint magnam velit, reiciendis molestias odio excepturi? Tempora temporibus facilis atque numquam.</p>
+
+<h2>Sit amet</h2>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur error dolor incidunt, consequuntur voluptas quod quo ab repudiandae. Voluptatem beatae delectus velit id, minus excepturi est temporibus quaerat quas dolorum?</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur mollitia inventore maiores obcaecati omnis, fugiat, eveniet delectus earum enim, placeat dolores cum neque laborum praesentium animi ipsum. Omnis, quisquam ullam!</p>
