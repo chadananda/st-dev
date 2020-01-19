55b608287e6114effc726ceb1235658c8dfe2187
David Hunt <git@dnotes.net>
2020-01-18 19:38:57 -0800

    slim header for pages other than front

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,17 +1,26 @@
 <script>
 	export let segment;
+	import { stores } from '@sapper/app';
+	const { preloading, page, session } = stores();
 </script>
 
-<header>
-	<img class='logo' src="logo_lotus.svg" alt='our logo: a lotus bloom'/>
-	<h1>Sacred Traditions</h1>
-</header>
+{#if $page.path === '/'}
+	<header class="front">
+		<img class='logo' src="logo_lotus.svg" alt='our logo: a lotus bloom'/>
+		<h1>Sacred Traditions</h1>
+	</header>
+{/if}
 
-<nav>
-	<a href="/">
-		<img class="logo_icon" src="/favicon.ico" alt="our logo: a lotus bloom" />
-		Home
-	</a>
+<nav class:front={$page.path === '/'} >
+	{#if $page.path !== '/'}
+		<header>
+			<a href="/">
+				<img class='logo' src="logo_lotus.svg" alt='our logo: a lotus bloom'/>
+				<h1>Sacred Traditions</h1>
+			</a>
+		</header>
+	{/if}
+
 	<a href="/ocean">
 		<img class="logo_ocean" src="/ocean_assets/favicon/favicon-32x32.png" alt="the world inside a drop" />
 		Ocean 2.0
@@ -23,20 +32,24 @@
 
 <style lang="scss">
 	@import "../style/theme.scss";
+
+	a {
+		text-decoration: none;
+	}
   nav {
 		position: -webkit-sticky; /* Safari */
 		position: sticky !important;
 		top: -4px;
 		z-index: 1000;
-		margin-top: 1em;
+		margin-top: -4px;
 		overflow: hidden;
 		border-color: $color-border;
 		box-shadow: 0px 2px 6px 0px rgba(184,191,255,0.49);
 		border-style: solid none;
 		border-width: .125rem;
-		text-align: center;
+		text-align: right;
 		background: $color-bg;
-		a {
+		>a {
 			font-size: 1rem;
 			padding: 1rem 2rem;
 			transition: all .15s ease-in;
@@ -50,25 +63,49 @@
 			}
 		}
 	}
-		header {
-			 margin: 0; padding:0; margin-top:1em;
-			 text-align: center;
+
+	header {
+		padding: 0;
+		text-align: center;
+		position: static;
+		img.logo {
+			width: 200px;
+			max-width: 50%;
+			margin: 0;
+			padding: 0;
 		}
-		header img.logo {
-		  width: 200px; max-width: 50%; margin: 0; padding:0;
+		h1 {
+			margin: 0;
+			font-weight: 100;
+			font-family: avenir next, avenir, sans-serif;
+			font-size: 3rem;
 		}
-		header h1 {
-			 margin:0;
-			 font-size:3rem;
-			 font-weight:100;
-			 font-family: avenir next, avenir, sans-serif;
+	}
+
+	nav.front {
+		text-align: center;
+	}
+	nav header {
+		position: absolute;
+		left: 1rem;
+		top: .7rem;
+		z-index: 1;
+		img.logo {
+			width: 44px;
+			display: inline-block;
 		}
-		img.logo_icon {
-			width: 20px; margin:0; padding: 0; margin-bottom: -8%; padding-right: .3em;
+		h1 {
+			font-size: 2rem;
+			display: inline-block;
+			vertical-align: bottom;
 		}
-		img.logo_ocean {
-			width: 18px; margin:0; padding: 0; margin-bottom: -4%; margin-right: .2em; opacity: .8;
-		}
+	}
+	img.logo_icon {
+		width: 20px; margin:0; padding: 0; margin-bottom: -8%; padding-right: .3em;
+	}
+	img.logo_ocean {
+		width: 18px; margin:0; padding: 0; margin-bottom: -4%; margin-right: .2em; opacity: .8;
+	}
  @media (max-width: 400px) {
    header h1 { font-size: calc(4vw + 4vh + 2vmin)}
 			header img {width: 100%}
