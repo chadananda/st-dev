04438ed8055bf6ef64102ec4df5817a03e06d2ac
David Hunt <git@dnotes.net>
2020-01-24 01:14:50 -0800

    fixed blog listing

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,7 +1,6 @@
 <script context="module">
 	export function preload({ params, query }) {
 		return this.fetch(`blog.json`).then(r => r.json()).then(content => {
-			console.log(content)
 			return { content }
 		});
 	}
@@ -9,12 +8,23 @@
 
 <script>
 	 export let content
+	 import Moment from 'moment'
 </script>
 
+<h1>Recent news</h1>
+
+<table>
 {#each content as item}
-   <!-- content here -->
-   <h3><a href="/blog/{item.meta.slug}">{item.meta.title}</a></h3>
+	<tr>
+		<td>{new Moment(item.meta.pubdate, 'YYYY-MM-DD').format('MMM D')}</td>
+		<td><a href="/blog/{item.meta.slug}">{item.meta.title}</a></td>
+	</tr>
 {:else}
    <!-- empty list -->
    <p>There are no blog posts available.</p>
 {/each}
+</table>
+
+<style lang="scss">
+	td { padding: .4em 1em; }
+</style>
