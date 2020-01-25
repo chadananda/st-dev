2aa347b4733ec92472884839a098decd5b9fbbd3
David Hunt <git@dnotes.net>
2020-01-25 03:19:40 -0800

    working on Facebook Page

---
+++ src/components/FacebookPage.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let page
+</script>
+
+<div id="fb-root"></div>
+<svelte:head>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0&appId=998903600487158"></script>
+</svelte:head>
+
