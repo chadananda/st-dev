43a51340c98cc34a5a1d0f5678be6557c3f44a10
Chad <chadananda@gmail.com>
2020-01-28 12:55:18 -0800

    cleaned up CSS for home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,66 +8,68 @@
 
 <div class="item news">
 	<h2>The Dawn-Breakers Challenge - 2020</h2>
-	<img src="/db-challenge/db-chall-jul-2019.jpg" alt="db challenge participants" />
+	<img class="floater invisible md:visible" src="/db-challenge/db-chall-jul-2019.jpg" alt="db challenge participants" />
 	<p>Last year we had a blast with a series of in-depth Dawn-Breakers study retreats (we even got a mention in the ABN).  So, by popular demand, we’re going to do it all over again. And we’re adding a few new programs to the mix. Check out our <a href="/courses">Summer 2020 retreat calendar!</a></p>
 	<p>Also, I finally have a good place to put a bunch of downloadable resources. A big thanks to Dropbox for motivating me -- by freezing my account due to too much download traffic.</p>
-	  <p class="list"><a href='/courses/dawnbreakers'>Dawn-Breakers Challenge Retreats 2020 →</a></p>
-		<img src="/db-challenge/class-study.jpg" alt="db challenge participants" style="width: 250px; transform: rotate(-2deg); margin-top: -5em;"/>
-   <p class="list"><a href='/dawn-breakers-challenge-materials'>Dawn-Breakers Challenge Downloadable Materials →</a></p>
-   <p class="list"><a href='/dawn-breakers-resources-additional'>Other DB Resources →</a></p>
-			<p class="list"><a href='https://bahaiteachings.org/dawn-breakers-challenge-history-bahai-heroes/' target="_blank">DB-Challenge article on bahai-teachings.org →</a></p>
-			<p class="list"><a href='/dawn-breakers-mailinglist'>Signup for Sacred-Retreats Announcement List! →</a></p>
+		<img class="floater invisible md:visible" src="/db-challenge/class-study.jpg" alt="db challenge participants" style="width: 200px; transform: rotate(-2deg); margin-top: -2em;"/>
+		<ul>
+				<li><a href='/courses/dawnbreakers'>Dawn-Breakers Challenge Retreats 2020 →</a></li>
+				<li><a href='/dawn-breakers-challenge-materials'>Dawn-Breakers Challenge Downloadable Materials →</a></li>
+				<li><a href='/dawn-breakers-resources-additional'>Other DB Resources →</a></li>
+				<li><a href='https://bahaiteachings.org/dawn-breakers-challenge-history-bahai-heroes/' target="_blank">DB-Challenge article on bahai-teachings.org →</a></li>
+				<li><a href='/dawn-breakers-mailinglist'>Signup for Sacred-Retreats Announcement List! →</a></li>
+		</ul>
 </div>
 
 
 
 <div class="item news">
 	<h2>Ocean 2.0 Interfaith Reader - updated</h2>
-	<img src="/ocean_assets/ocean-db.png" alt="Ocean reading dawn-breakers" style="width: 400px; transform: rotate(-1deg);" />
+	<img class="floater invisible md:visible" src="/ocean_assets/ocean-db.png" alt="Ocean reading dawn-breakers" style="width: 300px; transform: rotate(-1deg);" />
 	<p>This entire winter has been spent porting Ocean 2.0 to a faster and more lightweight framework. The advantage of this is that I get to keep Ocean in ‘beta’ status for yet another year. ;) </p>
-	  <p class="list"><a href='/courses/dawnbreakers'>Ocean 2.0 App Download Page</a></p>
-   <p class="list"><a href='/ocean'>Ocean Youtube videos</a></p>
-   <p class="list"><a href='/ocean'>Ocean 2.0 Facebook Group</a></p>
-			<p class="list"><a href='/ocean'>Ocean Twitter sharing</a></p>
+		<ul>
+				<li><a href='/courses/dawnbreakers'>Ocean 2.0 App Download Page</a></li>
+				<li><a href='/ocean'>Ocean Youtube videos</a></li>
+				<li><a href='/ocean'>Ocean 2.0 Facebook Group</a></li>
+				<li><a href='/ocean'>Ocean Twitter sharing</a></li>
+		</ul>
 </div>
 
 <div class="item news">
-
-	<img src="https://notepad.com/_nuxt/img/77d643c.gif" alt="db challenge participants" style="width: 250px; transform: rotate(2deg); margin-top:0em;" />
+	<img class="floater invisible md:visible" src="https://notepad.com/_nuxt/img/77d643c.gif" alt="db challenge participants" style="width: 250px; transform: rotate(2deg); margin-top:0em;" />
 	<h2>Immersion Arabic Language Lab - A crazy experiment... </h2>
 	<p>Also over a busy winter, we've been preparing an experimental Intensive Arabic retreat like no other. The aim is to get students from nearly zero to reading real Arabic literature in just a few days. If you are up for a serious immersion challenge, come and join us this summer! </p>
-	 <p class="list"><a href='/'>Immersion Arabic Boot Camp</a></p>
-		<p class="list"><a href='https://notepad.com/courses/' target="_blank">Online Prep Course</a></p>
+		<ul>
+				<li><a href='/'>Immersion Arabic Boot Camp</a></li>
+				<li><a href='https://notepad.com/courses/' target="_blank">Online Prep Course</a></li>
+		</ul>
 </div>
 
+
+
+
+
 <style>
-h1.title {
-	/* margin-left: 1em; margin-bottom: 1em; */
-}
-
-.item {
-  padding: 10px; padding-bottom:0;   background: rgb(249, 249, 252);
-  margin: 10px 20px 30px;
-    border-radius: 40px 20px!important;
-    border: 1px solid rgb(228, 228, 228);
-    box-shadow: 2px 2px 5px rgb(225, 225, 225);
-}
-.item h2 {margin-left: .5em;}
-.item p.list {margin-left: 1em; margin-top: 0;}
-.item p.list::before {
-	  content: " "; width: 20px; height: 20px; background: url('/link.svg');
-			background-size: contain;
-}
-
-.item img {
-	 width: 300px; float: right; margin-left: 1em; margin-top:-2em; margin-right: -5em; border: 1px solid rgb(232, 231, 231); padding: 10px; background: white;
-		padding-bottom: 15px;
-		box-shadow: 2px 2px 5px rgb(225, 225, 225);
-		transform: rotate(2deg);
-}
-
-.item.news {
-
-}
-
+	.item {
+			font-size: 1.2em;
+			padding: 10px; background: rgb(249, 249, 252);
+			margin: 10px 20px 30px;
+					border-radius: 20px 20px !important;
+					border: 1px solid rgb(228, 228, 228);
+					box-shadow: 2px 2px 5px rgb(225, 225, 225);
+			padding-right: 20%;
+	}
+	.item h2 { font-size: 1.4em; }
+	.item p {margin-bottom: .5em;}
+	.item a {text-decoration: underline; }
+	.item ul li { margin-left: 1em; margin-top: 3px;}
+	img.floater {
+			width: 300px; margin-left: 1em; margin-top:-2em; margin-right: -12em;
+			border: 1px solid rgb(232, 231, 231); padding: 5px; background: white;
+			padding-bottom: 10px;
+			box-shadow: 2px 2px 5px rgb(225, 225, 225);
+			transform: rotate(2deg);
+			float: right;
+			max-width:50%
+	}
 </style>
