699e8c0c1337b4879e935b8da0e73e7c966785c2
Chad <chadananda@gmail.com>
2020-01-29 09:45:13 -0800

    wrote a profound and lengthy GPDR-safe privacy policy

--- src/content/courses/dawnbreakers/dawnbreakers.md
+++ src/content/courses/dawnbreakers/dawnbreakers.md
@@ -6,19 +6,29 @@ teachers:
 facebook: https://www.facebook.com/The-Dawn-Breakers-Challenge-329820570524376/
 ---
 
-<img class="full" src="/db-challenge/class-study.jpg" alt="dawn-breakers challenge study class" />
+![dawn-breakers challenge study class](/db-challenge/class-study.jpg#full)
 
-## Study Retreat, 10 Days: The Dawn-Breakers Challenge 2020
+## 10-Day Study Retreat: The Dawn-Breakers Challenge 2020
 
-Please join us in Northern California this summer for a fun and intensive 10-day study of “The Dawn-Breakers” together with the primary doctrinal work of the Bahá'í Faith, “The Kitáb-i-Íqán.” As you know, the Guardian challenged us to “master” these books as a rock-foundation for effective teaching. Now that the Bicentennial of the bith of the Báb is upon us, it’s probably about time to get serious about doing just that.
+```
+Retreat Address: 1999 Ridge Rd, North San Juan, CA 95960
+ Contact Kaathlyn Connor at (530) 403 2997, kaathlyn.connor@gmail.com
+```
+<br>
 
-Together, we will read and discuss every page of the Dawn-Breakers, track every character, map every city and essentially rise to the Guardian’s challenge that we “...master and digest the facts recorded therein...” It’s going to be a lot of work and a lot of fun.
 
-Our outcome goals are clear: participants should know the stories and be able to identify the twenty-six key characters — as well as be able to accompany others in reading these important books once they return home.  As the Guardian promised us, “No one can read The Dawn-Breakers and remain cold and unreceptive.” Studying that book is a powerful spiritual journey — a quickening of the soul. It offers a revitalized Bahá’í identity and the inspiration we so badly need to get through these troubled days.
+Please join us in Northern California this summer for a fun and intensive 10-day study of “The Dawn-Breakers” together with the primary doctrinal work of the Bahá'í Faith, “The Kitáb-i-Íqán.” As you know, the Guardian challenged us to *“master”* these books as a rock-foundation for effective teaching. Now that the Bicentennial of the bith of the Báb is upon us, it’s probably about time to get serious about doing just that.
 
+Together, we will read and discuss every page of the Dawn-Breakers, track every character, map every city and essentially rise to the Guardian’s challenge that we *“...master and digest the facts recorded therein...”* It’s going to be a lot of work and a lot of fun.
+
+Our outcome goals are clear: participants should know the stories and be able to identify the twenty-six key characters — as well as be able to accompany others in reading these important books once they return home.  As the Guardian promised us, *“No one can read The Dawn-Breakers and remain cold and unreceptive.”* Studying that book is a powerful spiritual journey — a quickening of the soul.
+
+
+
+
+
+![group photo of dawn-breakers challenge 2019 participants](/db-challenge/db-chall-jul-2019.jpg#floater)
 ### Where will this be held?
-
-<img class="floater" src="/db-challenge/db-chall-jul-2019.jpg" alt="group photo of dawn-breakers challenge 2019 participants">
 
 In the mountains of Northern California, just East of Sacramento, our friends have a beautiful boarding-school facility with a gymnasium and comfortable classrooms. If you are not within driving distance, you can fly into the Sacramento Airport (SMF) or take a train to the Sacramento Amtrak train station. Our community volunteers will pick you up and shuttle you to the school. The school has various accommodation options, so families will usually be able to stay together.
 
@@ -26,6 +36,9 @@ In the mountains of Northern California, just East of Sacramento, our friends ha
 
 Every month in 2020, beginning with April. We know everyone has different schedules and commitments, so we thought, “Let’s just keep repeating every month, all year! Surely anyone who really wants to attend can find some month that works.”
 
+
+
+![participants dawn-breakers challenge 2019](/db-challenge/participants.jpg#floater)
 ### Who should attend? (ages 15-112)
 
 Given the pace and difficulty of material, only adults and youth 15 or older. Minors will need to be accompanied by a parent or a designated guardian. We will not be splitting out into groups so all participants should be mature enough to sustain a solid day of reading together.
@@ -34,17 +47,18 @@ Given the pace and difficulty of material, only adults and youth 15 or older. Mi
 
 The Guardian assures us that studying the Dawn-Breakers has a profound mystical effect on our dormant spiritual powers. It should not be a surprise therefore that Shoghi Effendi called on all the Baha’is (especially the youth) to master this book as a basis for teaching. He explained that it is not just a book of history, but a book of meditation. And he assured us that studying that book would quicken our dormant energies in a completely new way.
 
-But let’s be honest. It’s a very difficult book, Northern California is far away, 10 days is a lot of time and the demands of our material life fill up our calendars, sometimes years in advance.
-Nevertheless, the bicentennial is fast coming. So, if not now, then when?
+But let’s be honest. It’s a very difficult book, Northern California is far away, 10 days is a lot of time and the demands of our material life fill up our calendars, sometimes years in advance. Nevertheless, the bicentennial of the birth of the Báb is now upon us! So, if not now, then when?
 
-Whatever your plans are this summer we hope you will change them and join our little mountain retreat for a few days of spiritual adventure.
+Whatever your current plans are this summer, we hope you will change them and join our little mountain retreat for a few days of spiritual adventure.
 
+
+![bring Ocean 2.0](/db-challenge/bayan-db-challenge.jpg#floater)
 ### What to Bring? (books, light clothes, shampoo)
 
-Every student should bring their own copy of the Dawn-Breakers or a tablet or laptop computer with Ocean 2.0 Reader installed [http://bit.ly/ocean-app](/ocean). Books, supplies, bedding, food and all materials are supplied. You may want to bring a swimsuit for occasional outings to the nearby river.
+Every student should bring their own copy of the Dawn-Breakers or a tablet or laptop computer with [Ocean 2.0 Reader installed →](/ocean). Pens, pencils, notepads, bedding, towels, food etc. are supplied. We have plenty of laundry facilities as well. You may want to bring a swimsuit for occasional outings to the nearby river.
 
-### How much is it? (about $20/day.)
+### How much? ($320, about $30/day.)
 
-We're working hard to keep costs as low as possible -- and so far that seems to come to about $20/day for room, board and materials. Our costs are reduced because of the generosity of the community and some very dedicated volunteers. If you need any help getting to Sacramento, let us know and we’ll do whatever we can to get you here. If you’re driving and have extra room for a ride share, let us know.
+We're working hard to keep costs as low as possible — and so far that seems to come to about $30 per day for room, board and materials. Our costs are reduced because of the generosity of the community and some very dedicated volunteers. If you need any help getting to Sacramento, let us know and we’ll do whatever we can to get you here. If you’re driving and have extra room for a ride share, let us know.
 
 <br><br><br><br>
--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -1,1 +1,65 @@
-<h2> Privacy is pretty important  </h2>
+			<h1>Privacy Policy</h1>
+
+			<h3>Your privacy is pretty important to us.</h3>
+
+			Sacred Traditions, Inc. is located at:<br/>
+			<address class="ml-2 text-sm mb-2">
+			  Sacred Traditions, Inc.<br/>7890 Boren Ln. Granite Bay <br />95746 - California , United States<br/>9167656913
+   </address>
+
+			<p>It is Sacred Traditions, Inc.'s policy to respect your privacy regarding any information we may collect while operating our website. This Privacy Policy applies to <a href="http://sacred-traditions.org">sacred-traditions.org</a> (hereinafter, "us", "we", or "sacred-traditions.org"). We respect your privacy and are committed to protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what circumstances we may disclose the information to third parties. This Privacy Policy applies only to information we collect through the Website and does not apply to our collection of information from other sources.</p>
+
+			<p>This Privacy Policy, together with the Terms and conditions posted on our Website, set forth the general rules and policies governing your use of our Website. Depending on your activities when visiting our Website, you may be required to agree to additional terms and conditions.</p>
+
+						<h2>Website Visitors</h2>
+			<p>Like most website operators, Sacred Traditions, Inc. collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. Sacred Traditions, Inc.'s purpose in collecting non-personally identifying information is to better understand how Sacred Traditions, Inc.'s visitors use its website. From time to time, Sacred Traditions, Inc. may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.</p>
+
+			<p>Sacred Traditions, Inc. also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and for users leaving comments on http://sacred-traditions.org blog posts. Sacred Traditions, Inc. only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally-identifying information as described below.</p>
+
+						<h2>Gathering of Personally-Identifying Information</h2>
+			<p>Certain visitors to Sacred Traditions, Inc.'s websites choose to interact with Sacred Traditions, Inc. in ways that require Sacred Traditions, Inc. to gather personally-identifying information. The amount and type of information that Sacred Traditions, Inc. gathers depends on the nature of the interaction. For example, we ask visitors who sign up for a blog at http://sacred-traditions.org to provide a username and email address.</p>
+
+						<h2>Security</h2>
+			<p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+
+
+
+						<h2>Links To External Sites</h2>
+			<p>Our Service may contain links to external sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy and terms and conditions of every site you visit.</p>
+			<p>We have no control over, and assume no responsibility for the content, privacy policies or practices of any third party sites, products or services.</p>
+
+
+						<h2>Protection of Certain Personally-Identifying Information</h2>
+			<p>Sacred Traditions, Inc. discloses potentially personally-identifying and personally-identifying information only to those of its employees, contractors and affiliated organizations that (i) need to know that information in order to process it on Sacred Traditions, Inc.'s behalf or to provide services available at Sacred Traditions, Inc.'s website, and (ii) that have agreed not to disclose it to others. Some of those employees, contractors and affiliated organizations may be located outside of your home country; by using Sacred Traditions, Inc.'s website, you consent to the transfer of such information to them. Sacred Traditions, Inc. will not rent or sell potentially personally-identifying and personally-identifying information to anyone. Other than to its employees, contractors and affiliated organizations, as described above, Sacred Traditions, Inc. discloses potentially personally-identifying and personally-identifying information only in response to a subpoena, court order or other governmental request, or when Sacred Traditions, Inc. believes in good faith that disclosure is reasonably necessary to protect the property or rights of Sacred Traditions, Inc., third parties or the public at large.</p>
+
+			<p>If you are a registered user of http://sacred-traditions.org and have supplied your email address, Sacred Traditions, Inc. may occasionally send you an email to tell you about new features, solicit your feedback, or just keep you up to date with what's going on with Sacred Traditions, Inc. and our products. We primarily use our blog to communicate this type of information, so we expect to keep this type of email to a minimum. If you send us a request (for example via a support email or via one of our feedback mechanisms), we reserve the right to publish it in order to help us clarify or respond to your request or to help us support other users. Sacred Traditions, Inc. takes all measures reasonably necessary to protect against the unauthorized access, use, alteration or destruction of potentially personally-identifying and personally-identifying information.</p>
+
+						<h2>Aggregated Statistics</h2>
+			<p>Sacred Traditions, Inc. may collect statistics about the behavior of visitors to its website. Sacred Traditions, Inc. may display this information publicly or provide it to others. However, Sacred Traditions, Inc. does not disclose your personally-identifying information.</p>
+
+
+						<h2>Cookies</h2>
+			<p>To enrich and perfect your online experience, Sacred Traditions, Inc. uses "Cookies", similar technologies and services provided by others to display personalized content, appropriate advertising and store your preferences on your computer.</p>
+			<p>A cookie is a string of information that a website stores on a visitor's computer, and that the visitor's browser provides to the website each time the visitor returns. Sacred Traditions, Inc. uses cookies to help Sacred Traditions, Inc. identify and track visitors, their usage of http://sacred-traditions.org, and their website access preferences. Sacred Traditions, Inc. visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using Sacred Traditions, Inc.'s websites, with the drawback that certain features of Sacred Traditions, Inc.'s websites may not function properly without the aid of cookies.</p>
+
+			<p>By continuing to navigate our website without changing your cookie settings, you hereby acknowledge and agree to Sacred Traditions, Inc.'s use of cookies.</p>
+
+						<h2>E-commerce</h2>
+			<p>Those who engage in transactions with Sacred Traditions, Inc. – by purchasing Sacred Traditions, Inc.'s services or products, are asked to provide additional information, including as necessary the personal and financial information required to process those transactions. In each case, Sacred Traditions, Inc. collects such information only insofar as is necessary or appropriate to fulfill the purpose of the visitor's interaction with Sacred Traditions, Inc.. Sacred Traditions, Inc. does not disclose personally-identifying information other than as described below. And visitors can always refuse to supply personally-identifying information, with the caveat that it may prevent them from engaging in certain website-related activities.</p>
+
+
+			<h2>Privacy Policy Changes</h2>
+			<p>Although most changes are likely to be minor, Sacred Traditions, Inc. may change its Privacy Policy from time to time, and in Sacred Traditions, Inc.'s sole discretion. Sacred Traditions, Inc. encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.</p>
+
+
+			<h2>Contact Information</h2>
+				<p> If you have any questions about this Privacy Policy, please contact us via <a href="mailto:info@sacred-traditions.org">email</a> or <a href="tel:9167656913">phone</a>.</p>
+
+
+    <br><br><br>
+
+
+<style>
+  p {margin-bottom: 1em;}
+</style>
+
