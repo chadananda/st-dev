36438c550c79882b2f0263b1bf4e3b8c86c1421c
Chad <chadananda@gmail.com>
2020-01-28 20:47:35 -0800

    updated layout trying to get styles to work

--- src/routes/courses/_layout.svelte
+++ src/routes/courses/_layout.svelte
@@ -8,9 +8,9 @@
 
 <style lang="scss">
 
-		@import "../../style/global.scss";
+		// @import "../../style/global.scss";
 
-  main.courses {
+  .courses {
 				p { margin-bottom: .75em; font-size: 16px; }
 				img {
 						background:white; border: 1px solid #e2e8f0; padding: 5px !important; margin:5px;
