5b9a9ffe6ee460b545acafb2376cb19f64235c67
David Hunt <git@dnotes.net>
2020-02-02 14:09:54 -0800

    checkout: cart, buttons, modal show up

--- src/routes/courses/checkout.svelte
+++ src/routes/courses/checkout.svelte
@@ -1,25 +1,30 @@
 <h1 class="title">Registration Checkout</h1>
 
 {#each $cart as item,i}
-  <div class="ticket flex flex-col font-circus">
-    <div class="text-3xl overflow-hidden" style="line-height:1.25em; max-height:2.5em;">
+  <span class="ticket flex flex-col font-circus">
+    <span class="text-3xl text-center overflow-hidden" style="line-height:1.25em; max-height:2.5em;">
       <span class="text-base w-8 h-8 leading-loose inline-block text-center cursor-pointer float-right" on:click={(e) => {
         e.preventDefault()
         e.stopPropagation()
         cart.remove(i)
       }}>✖️</span>
       {item.session.Title}
-    </div>
-    <div class="flex-grow text-xl">{item.session.StartDateDisplay} – {item.session.EndDateDisplay}</div>
-    <div>Reserved:
+    </span>
+    <span class="flex-grow text-xl text-center">{item.session.StartDateDisplay} – {item.session.EndDateDisplay}</span>
+    <span>Reserved:
       <span class="w-3/4 h-12 overflow-hidden text-center inline-block border-b border-black text-3xl font-calligraphy">
         <span class="inline-block">{item.FirstName || ''}</span> <span class="inline-block">{item.LastName || ''}</span>
       </span>
-    </div>
-  </div>
+    </span>
+  </span>
 {:else}
   <p>Your cart is empty.</p>
 {/each}
+
+<div class="my-16 mx-auto" style="width:400px;">
+    <script src="https://www.paypal.com/sdk/js?client-id=ARLTZyWHyejtubwFnzlatVehD-WIp7wj-9Kfxfzj9YvPZVCB5e0W8Xe9LXf_we7NZ25OlGN_YxzVgKRr&currency=USD"></script>
+    <div id="paypal-buttons"></div>
+</div>
 
 {#if otherSessions.length}
   <h2>Want to stay a little longer?</h2>
@@ -27,10 +32,36 @@
   <SessionList sessions={otherSessions} />
 {/if}
 
-<div id="paypal-buttons" class:display-none={!$cart.length}></div>
+
+{#if submitVisible}
+  <div class="fixed inset-0 z-50 overflow-auto bg-smoke-light flex" on:click={() => submitVisible = false}>
+  <div class="relative p-8 bg-white w-full max-w-md m-auto flex-col flex" class:success={submitResponse.success}>
+    {#if !submitResponse}
+      <h3>Registering</h3>
+      <p>We're waiting for confirmation on your registration....</p>
+    {:else if submitResponse.success}
+      <h3 class="success">You are registered!</h3>
+      <p>Your registration has been received for the following people:</p>
+      <ul>
+      {#each submitResponse.data.people as p}
+        <li>{p}</li>
+      {/each}
+      </ul>
+      <p>Your reference code is <span style="font-family:monospace; font-weight:bold;">{submitResponse.data.reference}</span>.
+      You should also receive an email with this information.</p>
+    {:else if submitResponse.success === false}
+      <h3 class="error">Something went wrong!</h3>
+      <p>Please give us a call to sort things out.</p>
+    {/if}
+    <button type="button" class="center" on:click={() => submitVisible = false}>OK</button>
+  </div>
+  </div>
+{/if}
+
 
 <svelte:head>
-  <script src="https://www.paypal.com/sdk/js?client-id=ARLTZyWHyejtubwFnzlatVehD-WIp7wj-9Kfxfzj9YvPZVCB5e0W8Xe9LXf_we7NZ25OlGN_YxzVgKRr"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 </svelte:head>
 
 <script>
@@ -42,10 +73,23 @@
   // for adding on sessions around the same time
   import getSessions from '../../components/getSessions'
   import Moment from 'moment'
+  import { onMount } from 'svelte'
+  let dev = process.env.NODE_ENV !== 'production'
   let sessions = []
   let otherSessions = []
   let today = (new Date()).toISOString().split('T')[0]
   let startDates = {}, endDates = {}
+  let ref = getRef()
+  let payAmount = 'registration'
+  let submitResponse = false
+  let submitVisible = false
+  let total
+  function getTotal(amount = 'registration') {
+    return $cart.reduce((t, v) => {
+      return t + (amount === 'registration' ? v.registrationAmount : v.fullAmount)
+    },0)
+  }
+  $: total = getTotal(payAmount)
   $: for (let i=0; i<$cart.length; i++) {
     let StartDate = new Moment($cart[i].session.StartDate, 'YYYY-MM-DD')
     let EndDate = new Moment($cart[i].session.EndDate, 'YYYY-MM-DD')
@@ -73,37 +117,40 @@
     return ref
   }
 
-  if (process.browser && $cart.length) {
+  onMount(() => {
+    total = getTotal(payAmount)
     paypal.Buttons({
       createOrder: (data, actions) => {
-        purchase_units: [{
-          amount: {
-            currency_code: 'USD',
-            amount: (5 * $cart.length)
-          },
-          description: `Registration for ${$cart.count}`,
-          
-        }]
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              value: total
+            },
+            custom_id: `${ref}:${$cart[0].Email}`,
+            invoice_id: ref,
+          }]
+        })
       },
-    }).render('#paypal-buttons')
-  }
-
-    // fetch(url, {
-    //   method: "POST",
-    //   body: JSON.stringify(Object.assign({}, values, {testing: dev})),
-    // })
-    // .then(r => {
-    //   r.json().then(result => {
-    //     setSubmitting(false)
-    //     submitResponse = result
-    //     console.log(result)
-    //   })
-    // })
-    // .catch(e => {
-    //   setSubmitting(false)
-    //   submitResponse = e
-    //   console.log(e)
-    // })
+      onApprove: (data, actions) => {
+        submitVisible = true
+        actions.order.capture().then((details) => {
+          fetch(url, {
+            method: "POST",
+            body: JSON.stringify(Object.assign({}, $cart, {Ref: ref, testing: dev}))
+          })
+          .then(r => {
+            r.json().then(result => {
+              submitResponse = result
+            })
+          })
+        })
+        .catch(e => {
+          submitResponse = e
+          console.error(e)
+        })
+      }
+    }).render('#paypal-buttons');
+  })
 
 </script>
 
@@ -118,7 +165,7 @@
   width: 480px;
   margin: 0 auto .4em;
   padding: .6em 1em;
-  >div {
+  >span {
     text-align: center;
   }
 }
