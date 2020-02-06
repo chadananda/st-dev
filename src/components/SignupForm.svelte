8b88ad4c1614fc133b925dc5ece4747ab5d8bd03
David Hunt <git@dnotes.net>
2020-02-05 21:15:21 -0800

    SignupForm validation working well

--- src/components/SignupForm.svelte
+++ src/components/SignupForm.svelte
@@ -1,50 +1,57 @@
 <h2>Register for {course.meta.title}</h2>
 
-<form class="form max-w-full mx-auto" style="width:600px;" on:submit={handleSubmit}>
+<form class="form max-w-full mx-auto" style="width:600px;" on:submit|preventDefault|stopPropagation={handleSubmit} on:blur={validateForm} >
 
   {#if sessions.length > 1}
     <div class="field">
     <label>Session date</label>
-    <select name="StartDate" bind:value={values.StartDate} class="w-full" >
+    <select name="StartDate" bind:value={values.StartDate} class="w-full" class:error={errors.StartDate} on:change={validateForm} >
     
       <option disabled selected value="" class="italic grey-500">- please select a date -</option>
       {#each sessionOptions as item}
         <option disabled={item.disabled ? true : false} value={item.id}>{item.title}</option>
       {/each}
     </select>
+    {#if errors.StartDate} <p class="error">{errors.StartDate}</p> {/if}
     </div>
   {/if}
 
   <div class="field">
     <label>Name</label>
-    <input type="text" name="FullName" bind:value={values.FullName} autocomplete="name" class="w-full" on:change={(e) => {
-      let first = e.target.value.split(' ')
-      let last = ''
-      if (first.length > 1) last = first.splice(-1, 1)[0]
-      values.FirstName = first.join(' ') || ''
-      values.LastName = last || ''
-    }} />
+    <input type="text" name="FullName" bind:value={values.FullName} class:error={errors.FullName} autocomplete="name" class="w-full"
+      on:blur={validateForm}
+      on:change={(e) => {
+        let first = e.target.value.split(' ')
+        let last = ''
+        if (first.length > 1) last = first.splice(-1, 1)[0]
+        values.FirstName = first.join(' ') || ''
+        values.LastName = last || ''
+      }} />
+    {#if errors.FullName || errors.FirstName || errors.LastName} <p class="error">{errors.FullName} {errors.FirstName} {errors.LastName}</p> {/if}
   </div>
 
   <div class="field">
     <label>Email address</label>
-    <input type="email" name="Email" bind:value={values.Email} autocomplete="email" class="w-full" />
+    <input type="email" name="Email" bind:value={values.Email} class:error={errors.Email} autocomplete="email" class="w-full" on:blur={validateForm} />
+    {#if errors.Email} <p class="error">{errors.Email}</p> {/if}
   </div>
 
   <div class="field">
     <label>Phone number</label>
-    <input type="tel" name="Phone" bind:value={values.Phone} autocomplete="tel" class="w-full" on:keyup={formatPhone} />
+    <input type="tel" name="Phone" bind:value={values.Phone} class:error={errors.Phone} autocomplete="tel" class="w-full" on:keyup={formatPhone} on:blur={validateForm} />
+    {#if errors.Phone} <p class="error">{errors.Phone}</p> {/if}
   </div>
 
   <div class="field clearfix w-full">
     <label class="w-1/2 inline-block">How many people are in your party?</label>
-    <input class="w-12 inline-block text-right" type="text" name="People" bind:value={values.People} />
+    <input class="w-12 inline-block text-right" type="text" name="People" bind:value={values.People} class:error={errors.People} on:blur={validateForm} />
+    {#if errors.People} <p class="error">{errors.People}</p> {/if}
   </div>
 
   <div class="field clearfix options-chips">
     <label>Where do you plan to stay?</label>
     {#each housingOptions as item}
-      <input type="radio" bind:group={values.Housing} name="Housing" id="Housing-{item.id}" value="{item.id}" checked={item.id === 'dorm'} >
+      <input type="radio" bind:group={values.Housing} name="Housing" id="Housing-{item.id}" value="{item.id}" class:error={errors.Housing} checked={item.id === 'dorm'} on:blur={validateForm} >
       <label for="Housing-{item.id}">
         {item.title}
         {#if session && session.CourseID}
@@ -52,8 +59,10 @@
         {/if}
       </label>
     {/each}
-    {#if values.Housing === 'other'}
-      <p class="color-warning">Please be aware that there are NO hotels in the area; this option is only meant for local students.</p>
+    {#if errors.Housing}
+      <p class="error">{errors.Housing}</p>
+    {:else if values.Housing === 'other'}
+      <p class="warning">Please be aware that there are NO hotels in the area; this option is only meant for local students.</p>
     {/if}
   </div>
 
@@ -74,11 +83,11 @@
     the remainder may be paid now or upon arrival.</p>
   {/if}
 
-  <button class="big fab" type="submit" disabled={!session}>Add to cart</button>
+  <button class="big fab" type="submit" disabled={!session} >Add to cart</button>
 </form>
 
 {#if dev}
-  <Debug variable={{values, session}} />
+  <Debug variable={{values, errors}} />
 {/if}
 
 <script>
@@ -150,8 +159,9 @@
 
   // Form validation
   import {object, string, number} from 'yup';
+  let errors = {}
   let schema = object({
-    StartDate: string().default('').label('Session date').required().oneOf(sessionOptions.filter(s=>!s.disabled).map(s=>s.id)),
+    StartDate: string().default('').label('Session date').required().oneOf(sessionOptions.filter(s=>!s.disabled).map(s=>s.id), 'Please choose a session' ),
     FirstName: string().default('').label('Name').required().max(255),
     LastName: string().default('').label('Name').max(255),
     Email: string().default('').label('Email').required().email(),
@@ -186,14 +196,47 @@
     }
   }
 
+  // Form validation
+  function validateForm(event) {
+    let el = event.target
+    let path = el.getAttribute('name')
+    errors[path] = ''
+    schema.validate(values, {abortEarly:false}).catch(validationErrors => {
+      validationErrors.inner.forEach(err => {
+        if (err.path === path) errors[err.path] = err.message
+      })
+      console.error(validationErrors)
+    })
+  }
+
   // Form submit
   import url from './getUrl'
   import { goto } from '@sapper/app'
   function handleSubmit() {
-    if (session) {
-      cart.add(Object.assign(totals, {session}, values))
-      goto('/courses/checkout')
-    }
+    errors = {}
+    schema.validate(values, {abortEarly:false}).then(() => {
+      if (session) {
+        cart.add(Object.assign(totals, {session}, values))
+        goto('/courses/checkout')
+      }
+    }).catch(validationErrors => {
+      validationErrors.inner.forEach(err => {
+        errors[err.path] = err.message
+      })
+      console.error(validationErrors)
+    })
   }
 
 </script>
+
+<style lang="scss">
+  .error {
+    color: red;
+    border-color: red;
+  }
+  select.error { color: inherit; }
+  .warning {
+    background: yellow;
+    padding: .5em;
+  }
+</style>
