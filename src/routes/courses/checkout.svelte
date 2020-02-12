c058d4e8e6e06cc1946735b8e993644570bd3c16
David Hunt <git@dnotes.net>
2020-02-12 10:40:41 +0100

    fixed checkout total updates with cart, added full payment option

--- src/routes/courses/checkout.svelte
+++ src/routes/courses/checkout.svelte
@@ -1,34 +1,48 @@
 <h1 class="title">Registration Checkout</h1>
 
 {#if !submitVisible}
-{#each $cart as item,i}
-  <span class="ticket flex flex-col font-circus">
-    <span class="text-3xl text-center overflow-hidden" style="line-height:1.25em; max-height:2.5em;">
-      <span class="text-base w-8 h-8 leading-loose inline-block text-center cursor-pointer float-right" on:click={(e) => {
-        e.preventDefault()
-        e.stopPropagation()
-        cart.remove(i)
-      }}>✖️</span>
-      {item.session.Title}
-    </span>
-    <span class="flex-grow text-xl text-center">{item.session.StartDateDisplay} – {item.session.EndDateDisplay}</span>
-    <span>Reserved:
-      <span class="w-3/4 h-12 overflow-hidden text-center inline-block border-b border-black text-3xl font-calligraphy">
-        <span class="inline-block">{item.FirstName || ''}</span> <span class="inline-block">{item.LastName || ''}</span>
+  {#each $cart as item,i}
+    <span class="ticket flex flex-col font-circus">
+      <span class="text-3xl text-center overflow-hidden" style="line-height:1.25em; max-height:2.5em;">
+        <span class="text-base w-8 h-8 leading-loose inline-block text-center cursor-pointer float-right" on:click={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          cart.remove(i)
+        }}>✖️</span>
+        {item.session.Title}
+      </span>
+      <span class="flex-grow text-xl text-center">{item.session.StartDateDisplay} – {item.session.EndDateDisplay}</span>
+      <span>Reserved:
+        <span class="w-3/4 h-12 overflow-hidden text-center inline-block border-b border-black text-3xl font-calligraphy">
+          <span class="inline-block">{item.FirstName || ''}</span> <span class="inline-block">{item.LastName || ''}</span>
+        </span>
       </span>
     </span>
-  </span>
 
-{:else}
-  <p>Your cart is empty.</p>
-{/each}
+  {:else}
+    <p>Your cart is empty.</p>
+  {/each}
 
-{#if otherSessions.length}
-  <h2>Want to stay a little longer?</h2>
-  <p>Here are some other programs going on around when you will be here:</p>
-  <SessionList sessions={otherSessions} />
-{/if}
-<Debug variable={$cart} />
+  {#if $cart.length}
+    <span class="block my-4 mx-auto flex" style="width:400px;">
+      <span class="block text-2xl flex-none">Pay</span>
+      <span class="block flex-auto w-full">
+        <select style="margin-left:1em; margin-right:1em;" bind:value={payAmount}>
+          <option value="registration">registration only</option>
+          <option value="full">full amount now</option>
+        </select>
+      </span>
+      <span class="text-2xl flex-none">${total}</span>
+    </span>
+  {/if}
+
+  {#if otherSessions.length}
+    <h2>Want to stay a little longer?</h2>
+    <p>Here are some other programs going on around when you will be here:</p>
+    <SessionList sessions={otherSessions} />
+  {/if}
+
+  <Debug variable={$cart} />
 
 {:else}
   {#if !submitResponse}
@@ -58,7 +72,7 @@
 </div>
 
 <div class="my-16 mx-auto" class:hidden={submitVisible} style="width:400px;">
-    <div id="paypal-buttons"></div>
+  <div id="paypal-buttons"></div>
 </div>
 
 
@@ -81,73 +95,69 @@
   let otherSessions = []
   let today = (new Date()).toISOString().split('T')[0]
   let startDates = {}, endDates = {}
-  let ref = getRef()
+  let Ref = getRef()
   let payAmount = 'registration'
   let submitResponse = false
   let submitVisible = false
+  let checkoutVisible = false
+  let createOrder, onApprove
   let total
-  function getTotal(amount = 'registration') {
-    let newLineItems = []
-    total = $cart.reduce((t, v) => {
-      return t + (amount === 'registration' ? v.registrationAmount : v.fullAmount)
-    },0)
-  }
-  $: getTotal(payAmount)
+  $: total = $cart.reduce((t, v) => {
+    return t + (payAmount === 'registration' ? v.registrationAmount : v.fullAmount)
+  },0)
   $: cartKeys = $cart.map(s => s.session.Key)
   $: {
     otherSessions = $cart.reduce((acc, cur) => {
     return acc.concat((cur.session.prev || []).filter(s => s.StartDate >= today && cartKeys.indexOf(s.Key) === -1))
               .concat((cur.session.next || []).filter(s => s.StartDate >= today && cartKeys.indexOf(s.Key) === -1))
     }, [])
+    createOrder = (data, actions) => {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: total
+          },
+          custom_id: `${Ref}:${$cart[0].Email}`,
+          invoice_id: Ref,
+        }]
+      })
+    }
+    onApprove = (data, actions) => {
+      submitVisible = true
+      // NOTE: Errors from the Google Apps script are often returned in strange ways,
+      // e.g. a CORS error in the browser console, but the last request contains an HTML
+      // error report, which triggers the CORS error.
+      actions.order.capture().then((details) => {
+        fetch(url, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'text/plain'
+          },
+          body: JSON.stringify(Object.assign({}, {items: $cart, orderID: data.orderID, Ref, testing: dev}))
+        })
+        .then(r => {
+          cart.set([])
+          r.json().then(result => {
+            submitResponse = result
+          })
+        })
+      })
+      .catch(e => {
+        submitResponse = e
+        console.error(e)
+      })
+    }
   }
 
   function getRef() {
-    let ref = Math.random().toString(36).substring(2,8).toUpperCase()
-    if (!/[A-Z0-9]{6}/.test(ref)) return getRef()
-    return ref
+    let Ref = Math.random().toString(36).substring(2,8).toUpperCase()
+    if (!/[A-Z0-9]{6}/.test(Ref)) return getRef()
+    return Ref
   }
 
   onMount(() => {
     if ($cart.length) {
-      getTotal(payAmount)
-      paypal.Buttons({
-        createOrder: (data, actions) => {
-          return actions.order.create({
-            purchase_units: [{
-              amount: {
-                value: total
-              },
-              custom_id: `${ref}:${$cart[0].Email}`,
-              invoice_id: ref,
-            }]
-          })
-        },
-        onApprove: (data, actions) => {
-          submitVisible = true
-          // NOTE: Errors from the Google Apps script are often returned in strange ways,
-          // e.g. a CORS error in the browser console, but the last request contains an HTML
-          // error report, which triggers the CORS error.
-          actions.order.capture().then((details) => {
-            fetch(url, {
-              method: "POST",
-              headers: {
-                'Content-Type': 'text/plain'
-              },
-              body: JSON.stringify(Object.assign({}, {items: $cart, orderID: data.orderID, Ref: ref, testing: dev}))
-            })
-            .then(r => {
-              cart.set([])
-              r.json().then(result => {
-                submitResponse = result
-              })
-            })
-          })
-          .catch(e => {
-            submitResponse = e
-            console.error(e)
-          })
-        }
-      }).render('#paypal-buttons');
+      paypal.Buttons({createOrder,onApprove}).render('#paypal-buttons');
     }
   })
 
