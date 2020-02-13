b2729d82dc5ae8670d02ed7bda7b0087350376e7
David Hunt <git@dnotes.net>
2020-02-13 21:01:23 +0100

    solved json files not exporting

---
+++ src/routes/jsonlinks.svelte
@@ -0,0 +1,18 @@
+
+<ul>
+{#each links as a}
+  <li><a href="{a}">{a}</a></li>
+{/each}
+</ul>
+
+<script>
+export let links
+</script>
+
+<script context="module">
+  export async function preload() {
+    let articles = await this.fetch('/articles.json').then(r => r.json()).then(r => r.map(o => '/articles/' + o.slug + '.json'))
+    let courses = await this.fetch('/courses.json').then(r => r.json()).then(r => r.map(o => '/courses/' + o.meta.slug + '.json'))
+    return { links: [...articles, ...courses] }
+  }
+</script>
